50abd863d535a288ff99f761de46f9db651f1f43
Lucas Robson <returnnada@gmail.com>
2025-01-07 10:49:47 -0500

    Update

--- Updated-Website/src/TypeScript/App.tsx
+++ Updated-Website/src/TypeScript/App.tsx
@@ -1,15 +1,7 @@
-import { useState } from "react";
-import ListGroup from "./ListGroup";
 import NavBar from "./NavBar";
 import CardView from "./CardView";
 
 function App() {
-  let items = ["Item1", "Item2", "Item3", "Item4", "Item5"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <>
       <NavBar></NavBar>
