bc9338fb04f5ef77023ce62bfa22a80f98ad5389
Lucas Robson <returnnada@gmail.com>
2025-01-11 17:30:37 -0500

    Run it Back

--- Updated-Website/src/TypeScript/NavBar.tsx
+++ Updated-Website/src/TypeScript/NavBar.tsx
@@ -13,13 +13,13 @@ const NavigationBar = () => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <hr />
-            <Nav.Link className="hover-zoom" href="./Pages/HomePage">
+            <Nav.Link className="hover-zoom" href="/website/">
               Home
             </Nav.Link>
-            <Nav.Link className="hover-zoom" href="./Pages/AboutPage">
+            <Nav.Link className="hover-zoom" href="/website/About/">
               About
             </Nav.Link>
-            <Nav.Link className="hover-zoom" href="./Pages/ProjectPage">
+            <Nav.Link className="hover-zoom" href="/website/Projects/">
               Projects
             </Nav.Link>
             <Nav.Link className="hover-zoom" href="mailto:armaitus@gmail.com">
