d55cace7e0bfdf86c2e33ea4cbb9baddc9d3656c
Lucas Robson <returnnada@gmail.com>
2025-01-10 19:26:38 -0500

    Added 3D Models

--- Updated-Website/src/TypeScript/Pages/CircuitPage.tsx
+++ Updated-Website/src/TypeScript/Pages/CircuitPage.tsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function CircuitPage() {
   return (
     <div>
+      <Canvas>
+        <Suspense fallback={null}></Suspense>
+      </Canvas>
       <Link to={"/website/"}>Back</Link>
     </div>
   );
---
+++ Updated-Website/src/assets/gltf.d.ts
@@ -0,0 +1,5 @@
+declare module '*.gltf' {
+    const value: any;
+    export default value;
+}
+  
